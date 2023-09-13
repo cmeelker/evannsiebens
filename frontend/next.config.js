0b364e3fa9e17972083e858d1ae3fbf4b48b7200
Christa <christa@infi.nl>
2023-09-13 09:48:53 +0200

    Add images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,4 +11,19 @@ module.exports = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "evannsiebens-strapi-xtkdr.ondigitalocean.app",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,17 @@
 import axios from "axios";
+import Image from "next/image";
 export const dynamic = "force-dynamic";
+
+interface StrapiImage {
+  url: string;
+  alternativeText: string;
+}
 
 interface PortfolioItem {
   id: number;
   title: string;
   description: string;
+  image: StrapiImage;
 }
 
 const Home = async () => {
@@ -12,12 +19,20 @@ const Home = async () => {
 
   return (
     <>
-      <h1>Items:</h1>
+      <h1 className="text-4xl font-bold">Items:</h1>
       <ul>
         {portfolioItems.map((item: PortfolioItem) => (
           <li key={item.id}>
             <div>{item.title}</div>
             <div>{item.description}</div>
+            <div className="w-[400px] h-[400px] relative">
+              <Image
+                className="object-cover"
+                src={item.image.url}
+                alt={item.image.alternativeText}
+                fill={true}
+              />
+            </div>
           </li>
         ))}
       </ul>
@@ -29,12 +44,17 @@ export default Home;
 
 async function getPortFolioItems() {
   const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/portfolio-items`
+    `${process.env.NEXT_PUBLIC_API_URL}/api/portfolio-items?populate=*`
   );
+
   const portfolioItems: PortfolioItem[] = res.data.data.map((item: any) => ({
     id: item.id,
     title: item.attributes.Title,
     description: item.attributes.Description,
+    image: {
+      url: `${process.env.NEXT_PUBLIC_API_URL}${item.attributes.Image.data.attributes.url}`,
+      alternativeText: item.attributes.Image.data.attributes.alternativeText,
+    },
   }));
   return portfolioItems;
 }
