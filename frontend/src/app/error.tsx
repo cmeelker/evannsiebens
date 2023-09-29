958882a5aaf0c796fac1838cffc9875f6b19b027
Christa <christa@infi.nl>
2023-09-29 16:41:38 +0200

    Add GridList to navbar

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -1,5 +1,6 @@
 "use client"; // Error components must be Client Components
 
+import Layout from "@/components/Layout";
 import { useEffect } from "react";
 
 export default function Error({
@@ -15,7 +16,7 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
+    <Layout>
       <h2>Something went wrong!</h2>
       <button
         onClick={
@@ -25,6 +26,6 @@ export default function Error({
       >
         Try again
       </button>
-    </div>
+    </Layout>
   );
 }
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,26 +1,41 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-// TO DO: In CMS
+// TO DO: In CMS ?
 
 export default function NavBar() {
+  const pathname = usePathname();
+
+  const showListGrid = pathname === "/" || pathname === "/projects";
+  const listGridStyle = showListGrid ? "" : "hidden";
+
   return (
-    <nav className="text-h1 flex flex-col w-fit m-auto items-center">
-      <Link href="/" className="bg-bright-gray w-fit pt-3">
-        Evann Siebens
+    <div className="max-w-[88rem] m-auto flex justify-between">
+      <Link href="/projects" className={`text-nav h-fit ${listGridStyle}`}>
+        List
       </Link>
-      <ul className="bg-bright-gray flex pt-3">
-        <li>
-          <Link href="/bio">Bio</Link>
-        </li>
-        ⇉
-        <li>
-          <Link href="/cv">CV</Link>
-        </li>
-        ⇉
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+      <nav className="flex flex-col w-fit m-auto items-center">
+        <Link href="/" className="w-fit text-nav">
+          Evann Siebens
+        </Link>
+        <ul className="flex text-nav">
+          <li>
+            <Link href="/bio">Bio</Link>
+          </li>
+          ⇉
+          <li>
+            <Link href="/cv">CV</Link>
+          </li>
+          ⇉
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Link href="/" className={`text-nav h-fit ${listGridStyle}`}>
+        Grid
+      </Link>
+    </div>
   );
 }
