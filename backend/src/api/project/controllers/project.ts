e847ac4f255f9472e99926924cb396ac091883f4
Christa <christa@infi.nl>
2023-10-06 13:54:53 +0200

    Add slug endpoint to projects

--- backend/src/api/project/controllers/project.ts
+++ backend/src/api/project/controllers/project.ts
@@ -2,6 +2,19 @@
  * project controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::project.project');
+export default factories.createCoreController(
+  "api::project.project",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { slug } = ctx.params;
+      const entity = await strapi.db.query("api::project.project").findOne({
+        where: { slug },
+      });
+
+      const sanitizedEntity = await this.sanitizeOutput(entity);
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
