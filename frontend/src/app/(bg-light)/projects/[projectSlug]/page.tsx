73aacb3320ef3a6732ec60c08bc4f1caeba9dd06
Christa <christa@infi.nl>
2023-10-06 15:01:12 +0200

    Add metadata for project pages

--- frontend/src/app/(bg-light)/projects/[projectSlug]/page.tsx
+++ frontend/src/app/(bg-light)/projects/[projectSlug]/page.tsx
@@ -1,5 +1,6 @@
 import Richtext from "@/components/Richtext";
 import { getProject } from "@/services/projectService";
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 interface ProjectPageProps {
@@ -8,18 +9,27 @@ interface ProjectPageProps {
   };
 }
 
+export async function generateMetadata({
+  params,
+}: ProjectPageProps): Promise<Metadata> {
+  const project = await getProject(params.projectSlug);
+
+  return {
+    title: `Evann Siebens / ${project.title}`,
+  };
+}
+
 export default async function ProjectPage({ params }: ProjectPageProps) {
   const project = await getProject(params.projectSlug).catch(() => {
     redirect("/");
   });
   return (
-    <>
+    <div className="text-base md:text-lg">
       <h1>
         {project.title}, {project.year}
       </h1>
-      <div>
-        <Richtext text={project.description} />
-      </div>
-    </>
+
+      <Richtext text={project.description} />
+    </div>
   );
 }
