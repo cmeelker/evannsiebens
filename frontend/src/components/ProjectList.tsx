d87844e70f4985e299f0065eb94ce9212a3d34db
Christa <christa@infi.nl>
2023-10-07 16:26:34 +0200

    Add transition and fix hover glitch

--- frontend/src/components/ProjectList.tsx
+++ frontend/src/components/ProjectList.tsx
@@ -20,10 +20,11 @@ export default function ProjectList({ projects }: { projects: Project[] }) {
       <ul>
         {projects.map((project: Project) => {
           return (
-            <li key={project.id} className="hover:opacity-70">
+            <li key={project.id}>
               <Link
+                className="hover:opacity-70"
                 href={`/projects/${project.slug}`}
-                onMouseEnter={() => onMouseEnter(project.media[0])}
+                onMouseOver={() => onMouseEnter(project.media[0])}
                 onMouseLeave={() => setShowImage(false)}
               >
                 {project.title}, {project.year}
@@ -44,20 +45,20 @@ function HoverImage({
   showImage: boolean;
   image: StrapiImage;
 }) {
-  if (showImage) {
-    return (
-      <div className="w-[45%] xl:w-[55%] absolute top-0 right-0 h-full">
-        <div className="sticky top-0 w-full h-[25rem] xl:h-[30rem] lg:block hidden">
-          <Image
-            className="object-cover"
-            src={image.url}
-            alt={image.alt || ""}
-            fill={true}
-          />
-        </div>
+  const opacityClass = showImage ? "opacity-100" : "opacity-0";
+
+  return (
+    <div className="w-[45%] xl:w-[55%] absolute top-0 right-0 h-full pointer-events-none ">
+      <div
+        className={`sticky top-0 w-full h-[25rem] xl:h-[30rem] lg:block hidden transition-all duration-300 delay-100 ${opacityClass}`}
+      >
+        <Image
+          className="object-cover"
+          src={image.url}
+          alt={image.alt || ""}
+          fill={true}
+        />
       </div>
-    );
-  } else {
-    return <></>;
-  }
+    </div>
+  );
 }
