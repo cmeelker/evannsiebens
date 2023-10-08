3592dea5346bf783acba954acd6c4349cfc6a774
Christa <christa@infi.nl>
2023-10-08 18:28:22 +0200

    Add link style in richtext

--- frontend/src/components/Richtext.tsx
+++ frontend/src/components/Richtext.tsx
@@ -3,5 +3,7 @@ import Markdown from "react-markdown";
 export default function Richtext({ text }: { text: string }) {
   const formattedText = text.replace(/\n/gi, "&nbsp;  &nbsp;  \n");
 
-  return <Markdown className="break-words">{formattedText}</Markdown>;
+  return (
+    <Markdown className="break-words link-hover">{formattedText}</Markdown>
+  );
 }
