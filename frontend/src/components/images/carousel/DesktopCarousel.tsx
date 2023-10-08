fa80ae77f77d7d7f98ebe2e5267eced5b9efa185
Christa <christa@infi.nl>
2023-10-08 16:04:44 +0200

    Add functionality to close desktop carousel

--- frontend/src/components/images/carousel/DesktopCarousel.tsx
+++ frontend/src/components/images/carousel/DesktopCarousel.tsx
@@ -1,5 +1,5 @@
 import { StrapiImage } from "@/models/Image";
-import { Dispatch, SetStateAction, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import Slider from "react-slick";
 import Image from "next/image";
 
@@ -21,6 +21,8 @@ export default function DesktopCarousel({
       setCurrentSlide(newIndex);
     },
   };
+
+  const carouselRef = useRef<HTMLDivElement>(null);
   const sliderRef = useRef<Slider>(null);
   const nextSlide = () => {
     if (sliderRef.current) {
@@ -28,37 +30,58 @@ export default function DesktopCarousel({
     }
   };
 
-  // todo: animation toevoegen
-  if (!showCarousel) {
-    return <></>;
-  }
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        carouselRef.current &&
+        !carouselRef.current.contains(event.target as Node)
+      ) {
+        setShowCarousel(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const opacityClass = showCarousel
+    ? "opacity-100"
+    : "opacity-0 pointer-events-none";
 
   return (
-    <div className="w-screen h-screen bg-mirage/50 fixed top-0 left-0">
-      <div className="w-[60vw] h-[60vh] fixed top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2">
-        <Slider {...settings} ref={sliderRef}>
-          {images.map((image, index) => {
-            return (
-              <div key={index} className="w-full h-[60vh] relative">
-                <Image
-                  className="object-cover"
-                  src={image.url}
-                  alt={image.alt || ""}
-                  fill={true}
-                />
-              </div>
-            );
-          })}
-        </Slider>
-        <div className="flex justify-between -mt-2">
-          <div className="text-nav w-fit">
-            {images[currentSlide].alt || "Default description"}
+    <div className={`${opacityClass} transition-all duration-300`}>
+      <div className="w-screen h-screen bg-mirage/50 fixed top-0 left-0">
+        <div
+          ref={carouselRef}
+          className="w-[60vw] h-[60vh] fixed top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2"
+        >
+          <Slider {...settings} ref={sliderRef}>
+            {images.map((image, index) => {
+              return (
+                <div key={index} className="w-full h-[60vh] relative">
+                  <Image
+                    className="object-cover"
+                    src={image.url}
+                    alt={image.alt || ""}
+                    fill={true}
+                  />
+                </div>
+              );
+            })}
+          </Slider>
+          <div className="flex justify-between -mt-2">
+            <div className="text-nav w-fit">
+              {images[currentSlide].alt || "Default description"}
+            </div>
+            <CarouselNavigation
+              currentSlide={currentSlide}
+              nextSlide={nextSlide}
+              total={images.length}
+            />
           </div>
-          <CarouselNavigation
-            currentSlide={currentSlide}
-            nextSlide={nextSlide}
-            total={images.length}
-          />
         </div>
       </div>
     </div>
