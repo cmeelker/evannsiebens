8a2508bf0dfcfeffb6f30b99b0bb140ee1a212d3
Christa <christa@infi.nl>
2023-10-08 14:04:18 +0200

    Add mobile carousel

--- frontend/src/components/Richtext.tsx
+++ frontend/src/components/Richtext.tsx
@@ -3,5 +3,5 @@ import Markdown from "react-markdown";
 export default function Richtext({ text }: { text: string }) {
   const formattedText = text.replace(/\n/gi, "&nbsp;  &nbsp;  \n");
 
-  return <Markdown>{formattedText}</Markdown>;
+  return <Markdown className="break-words">{formattedText}</Markdown>;
 }
--- frontend/src/components/images/ImageCarousel.tsx
+++ frontend/src/components/images/ImageCarousel.tsx
@@ -1,3 +1,35 @@
-export default function ImageCarousel() {
-  <>Carousel</>;
+"use client";
+
+import { StrapiImage } from "@/models/Image";
+import Slider from "react-slick";
+import Image from "next/image";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function ImageCarousel({ media }: { media: StrapiImage[] }) {
+  var settings = {
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+  return (
+    <div className="w-screen mb-16 mt-2 -ml-3">
+      <Slider {...settings}>
+        {media.map((image, index) => {
+          return (
+            <div key={index} className="h-[20rem] relative">
+              <Image
+                className="object-cover"
+                src={image.url}
+                alt={image.alt || ""}
+                fill={true}
+              />
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+  );
 }
