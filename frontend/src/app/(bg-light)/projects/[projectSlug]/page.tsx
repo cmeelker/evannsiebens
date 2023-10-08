0e54537cce27abc35ef36dce6dd00545558e6b8d
Christa <christa@infi.nl>
2023-10-08 11:57:27 +0200

    Add images on project page

--- frontend/src/app/(bg-light)/projects/[projectSlug]/page.tsx
+++ frontend/src/app/(bg-light)/projects/[projectSlug]/page.tsx
@@ -1,7 +1,9 @@
 import Richtext from "@/components/Richtext";
+import { StrapiImage } from "@/models/Image";
 import { getProject } from "@/services/projectService";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 interface ProjectPageProps {
   params: {
@@ -24,12 +26,35 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
     redirect("/");
   });
   return (
-    <div className="text-base md:text-lg">
-      <h1>
-        {project.title}, {project.year}
-      </h1>
+    <div className="relative">
+      <div className="mr-8 w-[86%]">
+        <h1>
+          {project.title}, {project.year}
+        </h1>
 
-      <Richtext text={project.description} />
+        <Richtext text={project.description} />
+      </div>
+      <div className="w-[12%] flex flex-col items-end absolute top-0 right-0 ">
+        <ImageList images={project.media} />
+      </div>
     </div>
   );
+}
+
+function ImageList({ images }: { images: StrapiImage[] }) {
+  return images.map((image) => {
+    return (
+      <div
+        key={image.id}
+        className={`h-[6.4rem] w-full border bg-pink-400 transition-all duration-300 delay-100 hover:h-[31rem] hover:w-[51rem] relative`}
+      >
+        <Image
+          className="object-cover"
+          src={image.url}
+          alt={image.alt || ""}
+          fill={true}
+        />
+      </div>
+    );
+  });
 }
