e26de76e2126cce0aa4d53de93df780a21c16c52
Christa <christa@infi.nl>
2023-10-16 23:33:07 +0200

    Remove to do

--- frontend/src/clients/contentful.tsx
+++ frontend/src/clients/contentful.tsx
@@ -1,7 +1,5 @@
 import { createClient } from "contentful";
 
-// to do: keys in prod ENV
-
 export default function contentfulClient() {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID as string,
