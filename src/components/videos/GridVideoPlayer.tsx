ba8ec0387b1dc8c1551b9d387d96e97bbfed25bc
Christa <christa@infi.nl>
2023-11-06 19:18:46 +0100

    Add playsinline attribute to videos

--- src/components/videos/GridVideoPlayer.tsx
+++ src/components/videos/GridVideoPlayer.tsx
@@ -61,7 +61,13 @@ export function GridVideoPlayer({ video }: { video: ContentfulVideo }) {
       onMouseLeave={() => !isTouchDevice && Pause()}
       className="h-full w-full"
     >
-      <video ref={videoRef} loop muted className="h-full w-full object-cover">
+      <video
+        ref={videoRef}
+        loop
+        muted
+        playsInline
+        className="h-full w-full object-cover"
+      >
         <source src={video.url} type="video/mp4" title={video.caption} />
       </video>
     </div>
