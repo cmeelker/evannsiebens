d2f9127e4b59a8a3b77fff9e1d27bdd71dfac635
ChristaMMeelker <christa@infi.nl>
2024-01-21 10:50:43 +0100

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "@/style/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <NavBar />
         <div>{children}</div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
